2d1a3191cf666416d511e015660e27a7356faa46
Amine Yesilyurt <amine.yesilyurt@gmail.com>
2017-02-07 22:20:43 +0300

    Interface extending another interface.

---
+++ InterfaceExtendsInterface/src/amine/InterfaceExtendsInterface.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amine;
+
+/**
+ *
+ * @author Amine
+ */
+public class InterfaceExtendsInterface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        MyClass a = new MyClass();
+        a.methodOne();
+        a.methodTwo();
+        // TODO code application logic here
+    }
+    
+}
---
+++ InterfaceExtendsInterface/src/amine/MyClass.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amine;
+
+/**
+ *
+ * @author Amine
+ */
+public class MyClass implements InterfaceTwo{
+
+    @Override
+    public void methodTwo() {
+        System.out.println("methodTwo");
+    }
+
+    @Override
+    public void methodOne() {
+        System.out.println("methodOne");
+    }
+    
+    
+    
+}
